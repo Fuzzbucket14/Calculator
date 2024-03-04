6f53f9731ceaaeca4105bd8531077dd4ab0730b5
Fuzzbucket14 <159600680+Fuzzbucket14@users.noreply.github.com>
2024-03-04 08:42:33 -0600

    removed table.java and formated

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,142 +1,137 @@
 
- import java.util.Scanner;
- import java.io.FileWriter;
- import java.io.FileNotFoundException;
- import java.io.IOException;
- 
- public class Calculator {
- 
-     public void RunCalculateor(String Output) {
- 
-    try{
+import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
-       FileWriter myWriter = new FileWriter(Output);
-       Scanner myObj = new Scanner(System.in); 
-       boolean stay = true ;
- 
-       while (stay == true) {
- 
- 
-          try {
-             double Num1 = 0 ;
-             String op = "0" ;
-             double Num2 = 0 ;
-             
- 
-             //prombting input
-             System.out.println("enter equation"); 
- 
-             //writes println to output this happens to all println
-             myWriter.write("enter equation" + System.getProperty( "line.separator" )); 
- 
-             //starts decoding variables from scanner
+public class Calculator {
 
-             Num1 = myObj.nextDouble(); 
-             op = myObj.next();
- 
-             //skips num2 if squareroot is entered so it dosen't fuck up
-             if (!op.toUpperCase().contains("SQUAREROOT")){  
-                
-                Num2 = myObj.nextDouble(); 
- 
-             }
- 
-             //writes equation to output
-              myWriter.write(Num1 + " " + op + " " + Num2 + System.getProperty( "line.separator" )); 
-             double result = 1 ;           
- 
-             //big if, else if chain for operations
-             if (op.contains("+")) {                
- 
-               result = Num1 + Num2 ;
- 
-             } else if(op.contains("-")) {
-               
-               result = Num1 - Num2 ;
- 
-             } else if(op.contains("/")) {
-               
-               result = Num1 / Num2 ;
-                
-             } else if(op.contains("*")) {
-               
-                result = Num1 * Num2 ;
- 
-             }else if(op.contains("%")) {
-               
-               result = Num1 % Num2 ;
+   public void RunCalculateor(String Output) {
 
-            } else if(op.contains("^")) {
- 
-                double power = Num2 ;
-                result = 1 ;
-                while (power !=0) {
-                   result = result * Num1 ;
-                   power --;
-                }
-                 
-             } else if(op.toUpperCase().contains("SQUAREROOT")) {
-               
-               result = Math.sqrt(Num1);
- 
-             } 
-            //prints results and wrtites them to output
-            System.out.println("= " + result);
-            myWriter.write("= " + result + System.getProperty( "line.separator" ));
- 
-          }
- 
-          // if number is not entered correctly like if there is a letter this catchs that error and tells the user to do it correctly and writes it to output
-          catch(Exception E) {
- 
-             System.out.println("Make sure equation is entered correctly!");
-             myWriter.write("Make sure equation is entered correctly!"+ System.getProperty( "line.separator" ));
- 
-          }//finally statement prombts user to continue or not
-          finally {
-             boolean exit = false ;
-             
-             while (exit==false) {
-                
-             System.out.println("Exit? Y/N") ;
-            
-             myWriter.write("Exit? Y/N" + System.getProperty( "line.separator" ));
-            
-             myObj.nextLine();
- 
-             String Exit = myObj.next();
- 
-             myWriter.write(Exit + System.getProperty( "line.separator" ));
- 
-             if (Exit.equalsIgnoreCase("Y")) {
-                exit = true ;
-                stay = false; 
- 
- 
-             }else if (Exit.equalsIgnoreCase("N")) {
-                exit = true;
-                myObj.nextLine();
-             }else {
-                System.out.println("Please enter Y or N");
-                myWriter.write("Please enter Y or N" + System.getProperty( "line.separator" ));
-             }
-           }
-          }
-       }
-       //closes scanner and writer
-       myObj.close() ;  
-       myWriter.close();
- 
-       //theese are here for write errors
-     } catch (FileNotFoundException e) {
+      try {
+
+         FileWriter myWriter = new FileWriter(Output);
+         Scanner myObj = new Scanner(System.in);
+         boolean stay = true;
+
+         while (stay == true) {
+
+            try {
+               double Num1 = 0;
+               String op = "0";
+               double Num2 = 0;
+
+               // prombting input
+               System.out.println("enter equation");
+
+               // writes println to output this happens to all println
+               myWriter.write("enter equation" + System.getProperty("line.separator"));
+
+               // starts decoding variables from scanner
+
+               Num1 = myObj.nextDouble();
+               op = myObj.next();
+
+               // skips num2 if squareroot is entered so it dosen't fuck up
+               if (!op.toUpperCase().contains("SQUAREROOT")) {
+
+                  Num2 = myObj.nextDouble();
+
+               }
+
+               // writes equation to output
+               myWriter.write(Num1 + " " + op + " " + Num2 + System.getProperty("line.separator"));
+               double result = 1;
+
+               // big if, else if chain for operations
+               if (op.contains("+")) {
+
+                  result = Num1 + Num2;
+
+               } else if (op.contains("-")) {
+
+                  result = Num1 - Num2;
+
+               } else if (op.contains("/")) {
+
+                  result = Num1 / Num2;
+
+               } else if (op.contains("*")) {
+
+                  result = Num1 * Num2;
+
+               } else if (op.contains("%")) {
+
+                  result = Num1 % Num2;
+
+               } else if (op.contains("^")) {
+
+                  double power = Num2;
+                  result = 1;
+                  while (power != 0) {
+                     result = result * Num1;
+                     power--;
+                  }
+
+               } else if (op.toUpperCase().contains("SQUAREROOT")) {
+
+                  result = Math.sqrt(Num1);
+
+               }
+               // prints results and wrtites them to output
+               System.out.println("= " + result);
+               myWriter.write("= " + result + System.getProperty("line.separator"));
+
+            }
+
+            // if number is not entered correctly like if there is a letter this catchs that
+            // error and tells the user to do it correctly and writes it to output
+            catch (Exception E) {
+
+               System.out.println("Make sure equation is entered correctly!");
+               myWriter.write("Make sure equation is entered correctly!" + System.getProperty("line.separator"));
+
+            } // finally statement prombts user to continue or not
+            finally {
+               boolean exit = false;
+
+               while (exit == false) {
+
+                  System.out.println("Exit? Y/N");
+
+                  myWriter.write("Exit? Y/N" + System.getProperty("line.separator"));
+
+                  myObj.nextLine();
+
+                  String Exit = myObj.next();
+
+                  myWriter.write(Exit + System.getProperty("line.separator"));
+
+                  if (Exit.equalsIgnoreCase("Y")) {
+                     exit = true;
+                     stay = false;
+
+                  } else if (Exit.equalsIgnoreCase("N")) {
+                     exit = true;
+                     myObj.nextLine();
+                  } else {
+                     System.out.println("Please enter Y or N");
+                     myWriter.write("Please enter Y or N" + System.getProperty("line.separator"));
+                  }
+               }
+            }
+         }
+         // closes scanner and writer
+         myObj.close();
+         myWriter.close();
+
+         // theese are here for write errors
+      } catch (FileNotFoundException e) {
          e.printStackTrace();
-     
-    }catch (IOException e) {
-        e.printStackTrace();
-     }
 
- }
+      } catch (IOException e) {
+         e.printStackTrace();
+      }
+
+   }
 }
- 
- 
- 
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,63 +8,53 @@
 //Table Tabe = new Table();
 //Tabe.RunTable(args);
 
-
 import java.text.SimpleDateFormat;
 import java.io.File;
 import java.util.Date;
 import java.io.FileWriter;
 import java.io.IOException;
 
-
-
-
 public class Main {
 
-    public static void main(String[] args) {
+   public static void main(String[] args) {
 
-      try{
+      try {
 
-       //finds date and time for output file
-       Date currentdate = new Date();
-       SimpleDateFormat formatter = new SimpleDateFormat("_dd_MM_yyyy_HH_mm_ss");
-       String datetime = formatter.format(currentdate);
-       String date = datetime;
- 
-       //makes file directory string
-       String Output = System.getProperty("user.dir") + "/output";
- 
-       //makes file directory
-       new File(Output).mkdirs();
- 
-       //adds extra /output for output file
-       Output = Output + "/output" ;
- 
-       //makes output file
+         // finds date and time for output file
+         Date currentdate = new Date();
+         SimpleDateFormat formatter = new SimpleDateFormat("_dd_MM_yyyy_HH_mm_ss");
+         String datetime = formatter.format(currentdate);
+         String date = datetime;
 
-       String outputfolder = Output + "Output"+date+".txt";
-       File file = new File(outputfolder); 
-       FileWriter myWriter = new FileWriter(outputfolder);
- 
-       //tells user taht we made a new file
-       if (file.createNewFile()) {
-        System.out.println("created output file:" + Output+date+".txt");
-        myWriter.write("created output file:" + Output+date+".txt" + System.getProperty( "line.separator" ));
-       }
+         // makes file directory string
+         String Output = System.getProperty("user.dir") + "/output";
 
+         // makes file directory
+         new File(Output).mkdirs();
 
-       //runs Calculator
-       Calculator calc = new Calculator();
-       calc.RunCalculateor(outputfolder);
+         // adds extra /output for output file
+         Output = Output + "/output";
 
-      myWriter.close();
-      }catch (IOException e) {
+         // makes output file
+
+         String outputfolder = Output + "Output" + date + ".txt";
+         File file = new File(outputfolder);
+         FileWriter myWriter = new FileWriter(outputfolder);
+
+         // tells user taht we made a new file
+         if (file.createNewFile()) {
+            System.out.println("created output file:" + Output + date + ".txt");
+            myWriter.write("created output file:" + Output + date + ".txt" + System.getProperty("line.separator"));
+         }
+
+         // runs Calculator
+         Calculator calc = new Calculator();
+         calc.RunCalculateor(outputfolder);
+
+         myWriter.close();
+      } catch (IOException e) {
          e.printStackTrace();
       }
 
-
-       
-      
    }
 }
-
-
